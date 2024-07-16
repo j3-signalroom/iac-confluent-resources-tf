08947b701fe5bb90390e0f87065adaaeed72a25f
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2024-07-16 18:15:32 -0400

    Refactored.

---
+++ .terraform.lock.hcl
@@ -0,0 +1,68 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/confluentinc/confluent" {
+  version     = "1.80.0"
+  constraints = "1.80.0, ~> 1.80.0"
+  hashes = [
+    "h1:ya+/eWacrI94sOtKud4B7lKxCaVtnB1SOu1rZyteOpg=",
+    "zh:068c86b69e37c84c64ebe68140e77386a804634d340664af5ed1ca62da6fd53d",
+    "zh:06e142d64f7d4a4749b4de61f9023e151c9f89f2010d4fb7df9059bbce9658e1",
+    "zh:0b4a6e7c26c94775181989af4961013242317d74e427e65bfab5e03284be6d91",
+    "zh:1784b2922d8a372f35b36bddae163300a8b97d3ea836c31f525ecbc606d4dde8",
+    "zh:2cff7d29e430ce869363bd9bf4fad0ca29a4f23646224d71d78ca6ca31367582",
+    "zh:53e011bfa982f9b8e51d5f15e45ece4b0bd3a81c7844fab814e4d0eb5d50a6dc",
+    "zh:6820d9025ae3edb14b8cf3b3b24e622789d31430ccde2ed420f65bfc1785bd0c",
+    "zh:793de6d535456a2af8489934ca2c2559d1a1956550462ab69be1742bb2b47983",
+    "zh:b862eb08d748a9636802bf7eebd65dcf1243f75e12b64c7dc6bde7812bbdcba9",
+    "zh:b97b2fd1d90645a68ad8ef79b47b788c79486c53490752fc894b8ff0d39e3132",
+    "zh:d5ff7c6fe6dc4b61752d4ac29c6a7ba0c355b9f9a6fa727482a3ff27554715d4",
+    "zh:db5bc09b2c1db4d47e35a127ba04e6f1b98fabf76eea60f5b07253634068989a",
+    "zh:ea819f8764164908f8cbaed65b01ebc478630901a98e3662f6bd234a4769db91",
+    "zh:eac21afe5405ce179b3c671a06da0a61f81a69e3c0bdb0f69960be40b14eb080",
+    "zh:f15bd76d38dd2a8fa55fef31741cd7507b08cf045c03e7a818e46abe4daf1882",
+    "zh:f55c0fbc9bd0321a2b782d348a41f7af43657e44302ad4705ae81a5d24f87e06",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version     = "5.58.0"
+  constraints = "~> 5.58.0"
+  hashes = [
+    "h1:XnAwb/MGeP7sxz/0SKLQF1ujaP7Bg15ol+ca7KZruio=",
+    "zh:15e9be54a8febe8e560362b10967cb60b680ca3f78fe207d7209b76e076f59d3",
+    "zh:240f6899a2cec259aa2729ce031f6af2b453f90a8b59118bb2571c54acc65db8",
+    "zh:2b6e8e2ab1a3dce1001503dba6086a128bb2a71652b0d0b3b107db665b7d6881",
+    "zh:579b0ed95247a0bd8bfb3fac7fb767547dde76026c578f4f184b5743af5e32cc",
+    "zh:6adcd10fd12be0be9eb78a89e745a5b77ae0d8b3522cd782456a71178aad8ccb",
+    "zh:7f829cef82f0a02faa97d0fbe1417a40b73fc5142e883b12eebc5b71015efac9",
+    "zh:81977f001998c9096f7b59710996e159774a9313c1bc03db3beb81c3e016ebef",
+    "zh:9b12af85486a96aedd8d7984b0ff811a4b42e3d88dad1a3fb4c0b580d04fa425",
+    "zh:a5d98ac6fab6e6c85164ca7dd38f94a1e44bd70c0e8354c61f7fbabf698957cd",
+    "zh:c27fa4fed50f6f83ca911bef04f05d635a7b7a01a89dc8fc5d66a277588f08df",
+    "zh:d4042bdf86ca6dc10e0cca91c4fcc592b12572d26185b3d37bbbb9e2026ac68b",
+    "zh:d536482cf4ace0d49a2a86c931150921649beae59337d0c02a785879fe943cf3",
+    "zh:e205f8243274a621fb9ef2b5e2c71e84c1670be1d23697739439f5a831fa620f",
+    "zh:eb76ce0c77fd76c47f57122c91c4fcf0f72c01423538ed7833eaa7eeaae2edf6",
+    "zh:ffe04e494af6cc7348ceb8d85f4c1d5a847a44510827b4496513c810a4d9196d",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/time" {
+  version = "0.11.2"
+  hashes = [
+    "h1:qg3O4PmHnlPcvuZ2LvzOYEAPGOKtccgD5kPdQPZw094=",
+    "zh:02588b5b8ba5d31e86d93edc93b306bcbf47c789f576769245968cc157a9e8c5",
+    "zh:088a30c23796133678d1d6614da5cf5544430570408a17062288b58c0bd67ac8",
+    "zh:0df5faa072d67616154d38021934d8a8a316533429a3f582df3b4b48c836cf89",
+    "zh:12edeeaef96c47f694bd1ba7ead6ccdb96028b25df352eea4bc5e40de7a59177",
+    "zh:1e859504a656a6e988f07b908e6ffe946b28bfb56889417c0a07ea9605a3b7b0",
+    "zh:64a6ae0320d4956c4fdb05629cfcebd03bcbd2206e2d733f2f18e4a97f4d5c7c",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:924d137959193bf7aee6ebf241fbb9aec46d6eef828c5cf8d3c588770acae7b2",
+    "zh:b3cc76281a4faa9c2293a2460fc6962f6539e900994053f85185304887dddab8",
+    "zh:cbb40c791d4a1cdba56cffa43a9c0ed8e69930d49aa6bd931546b18c36e3b720",
+    "zh:d227d43594f8cb3d24f1fdd71382f14502cbe2a6deaddbc74242656bb5b38daf",
+    "zh:d4840641c46176bb9d70ba3aff09de749282136c779996b546c81e5ff701bbf6",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,190 @@
+terraform {
+    cloud {
+        organization ="<TERRAFORM CLOUD ORGANIZATION NAME>"
+
+        workspaces {
+            name = "<TERRAFORM CLOUD ORGANIZATION's WORKSPACE NAME>"
+        }
+  }
+
+  required_providers {
+        confluent = {
+            source  = "confluentinc/confluent"
+            version = "~> 1.80.0"
+        }
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 5.58.0"
+        }
+    }
+}
+
+provider "confluent" {
+  cloud_api_key    = var.confluent_cloud_api_key
+  cloud_api_secret = var.confluent_cloud_api_secret
+}
+
+provider "aws" {
+    region  = var.aws_region
+}
+
+locals {
+  cloud          = "AWS"
+  secrets_prefix = "/confluent_cloud_resource"
+}
+
+# Reference the signalRoom Confluent Cloud Organization
+data "confluent_organization" "env" {}
+
+# Create the Confluent Cloud Environment
+resource "confluent_environment" "env" {
+    display_name = "signal-intelligence-${var.aws_profile}"
+
+    #lifecycle {
+    #  prevent_destroy = true
+    #}
+}
+
+# Create the Service Account for the Kafka Cluster data-ingestion API
+resource "confluent_service_account" "schema_registry_api" {
+    display_name = "demo-${var.aws_profile}-environment-api"
+    description  = "Demo Environment API Service Account"
+}
+
+# Config the environment's schema registry
+data "confluent_schema_registry_region" "env" {
+    cloud   = local.cloud
+    region  = var.aws_region
+    package = "ESSENTIALS"
+}
+
+resource "confluent_schema_registry_cluster" "env" {
+  package = data.confluent_schema_registry_region.env.package
+
+  environment {
+    id = confluent_environment.env.id
+  }
+
+  region {
+    # See https://docs.confluent.io/cloud/current/stream-governance/packages.html#stream-governance-regions
+    # Schema Registry and Kafka clusters can be in different regions as well as different cloud providers,
+    # but you should to place both in the same cloud and region to restrict the fault isolation boundary.
+    # A fault isolation boundary, also known as a swimlane, is a concept that separates services into failure 
+    # domains to limit the impact of a failure to a specific number of components.
+    id = data.confluent_schema_registry_region.env.id
+  }
+
+  #lifecycle {
+  #  prevent_destroy = true
+  #}
+}
+
+# Create the Environment API Key Pairs, rotate them in accordance to a time schedule, and provide the current
+# acitve API Key Pair to use
+module "schema_registry_api_key_rotation" {
+    source  = "github.com/j3-signalroom/j3-iac-confluent_cloud_resource_api_key_rotation-tf"
+
+    # Required Input(s)
+    owner = {
+        id          = confluent_service_account.schema_registry_api.id
+        api_version = confluent_service_account.schema_registry_api.api_version
+        kind        = confluent_service_account.schema_registry_api.kind
+    }
+
+    resource = {
+        id          = confluent_schema_registry_cluster.env.id
+        api_version = confluent_schema_registry_cluster.env.api_version
+        kind        = confluent_schema_registry_cluster.env.kind
+
+        environment = {
+            id = confluent_environment.env.id
+        }
+    }
+
+    confluent_cloud_api_key    = var.confluent_cloud_api_key
+    confluent_cloud_api_secret = var.confluent_cloud_api_secret
+
+    # Optional Input(s)
+    key_display_name = "Confluent Schema Registry Cluster Service Account API Key - {date} - Managed by Terraform Cloud"
+    number_of_api_keys_to_retain = var.number_of_api_keys_to_retain
+    day_count = var.day_count
+}
+
+# Create the Data Ingestion Kafka cluster
+resource "confluent_kafka_cluster" "demo_kafka_cluster" {
+    display_name = "demo"
+    availability = "SINGLE_ZONE"
+    cloud        = local.cloud
+    region       = var.aws_region
+    basic {}
+
+    environment {
+        id = confluent_environment.env.id
+    }
+}
+
+# Create the Service Account for the Kafka Cluster data-ingestion API
+resource "confluent_service_account" "demo_kafka_cluster_api" {
+    display_name = "demo-${var.aws_profile}-kafka_cluster_data_ingestion-api"
+    description  = "Demo Kafka Cluster API Service Account"
+}
+
+# Create the Kafka Cluster API Key Pairs, rotate them in accordance to a time schedule, and provide the current acitve API Key Pair
+# to use
+module "demo_kafka_cluster_api_key_rotation" {
+    source  = "github.com/j3-signalroom/j3-iac-confluent_cloud_resource_api_key_rotation-tf"
+
+    #Required Input(s)
+    owner = {
+        id          = confluent_service_account.demo_kafka_cluster_api.id
+        api_version = confluent_service_account.demo_kafka_cluster_api.api_version
+        kind        = confluent_service_account.demo_kafka_cluster_api.kind
+    }
+
+    resource = {
+        id          = confluent_kafka_cluster.demo_kafka_cluster.id
+        api_version = confluent_kafka_cluster.demo_kafka_cluster.api_version
+        kind        = confluent_kafka_cluster.demo_kafka_cluster.kind
+
+        environment = {
+            id = confluent_environment.env.id
+        }
+    }
+
+    confluent_cloud_api_key    = var.confluent_cloud_api_key
+    confluent_cloud_api_secret = var.confluent_cloud_api_secret
+
+    # Optional Input(s)
+    key_display_name = "Confluent Data Ingestion Kafka Cluster Service Account API Key - {date} - Managed by Terraform Cloud"
+    number_of_api_keys_to_retain = var.number_of_api_keys_to_retain
+    day_count = var.day_count
+}
+
+# Create the Schema Registry Secrets: API Key Pair and REST endpoint
+resource "aws_secretsmanager_secret" "schema_registry_api_key" {
+    name = "${local.secrets_prefix}/schema_registry"
+    description = "signalRoom Intelligence Schema Registry secrets"
+}
+
+resource "aws_secretsmanager_secret_version" "schema_registry_api_key" {
+    secret_id     = aws_secretsmanager_secret.schema_registry_api_key.id
+    secret_string = jsonencode({"api_key": "${module.schema_registry_api_key_rotation.active_api_key.id}", 
+                                "api_secret": "${module.schema_registry_api_key_rotation.active_api_key.secret}",
+                                "rest_endpoint": "${confluent_schema_registry_cluster.env.rest_endpoint}"})
+}
+
+# Create the Kafka Cluster Secrets: API Key Pair, JAAS (Java Authentication and Authorization) representation,
+# bootstrap server URI and REST endpoint
+resource "aws_secretsmanager_secret" "demo_kafka_cluster_api_key" {
+    name = "${local.secrets_prefix}/data-ingestion-kafka-cluster"
+    description = "signalRoom Intelligence Data Ingestion Kafka Cluster secrets"
+}
+
+resource "aws_secretsmanager_secret_version" "demo_kafka_cluster_api_key" {
+    secret_id     = aws_secretsmanager_secret.demo_kafka_cluster_api_key.id
+    secret_string = jsonencode({"api_key": "${module.demo_kafka_cluster_api_key_rotation.active_api_key.id}", 
+                                "api_secret": "${module.demo_kafka_cluster_api_key_rotation.active_api_key.secret}",
+                                "java_client_jaas_configuration": "org.apache.kafka.common.security.plain.PlainLoginModule required username='${module.demo_kafka_cluster_api_key_rotation.active_api_key.id}' password='${module.demo_kafka_cluster_api_key_rotation.active_api_key.secret}';",
+                                "bootstrap_url": "${confluent_kafka_cluster.demo_kafka_cluster.bootstrap_endpoint}"
+                                "rest_endpoint": "${confluent_kafka_cluster.demo_kafka_cluster.rest_endpoint}"})
+}
